53ea041a0f1abbd9378e0991235d01cef07f2e8c
saipavan954 <saipavantirumani883@gmail.com>
2026-09-26 14:21:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (56.29%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -2,7 +2,7 @@ class Solution:
     def addDigits(self, num: int) -> int:
         while num >=10:
             sum=0
-            while num>0:
+            while num!=0:
                 sum += num%10
                 num//=10
             num=sum
